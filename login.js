bfc24b7d514606ad2a235895c003e61d9a966a3c
Lucy-Tsai <acc2008e14@yahoo.com.tw>
2023-07-28 17:16:07 +0800

    　new

---
+++ login.js
@@ -0,0 +1,7 @@
+$("#forgotpassword").on("click", function() {
+    window.location.href = "forgotPassword.html"; // 跳轉至忘記密碼頁面
+});
+
+$("#register").on("click", function() {
+    window.location.href = "register.html"; // 跳轉至忘記密碼頁面
+});
